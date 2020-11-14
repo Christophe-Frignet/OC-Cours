c38b3ff19471ae55ee454e22e4d1a564786e5d4b
Christophe Frignet <chrisfrignet@gmail.com>
2020-11-14 16:49:55 +0100

    Début isolation de l'accès aux données

--- tp-blog-MVC/index.php
+++ tp-blog-MVC/index.php
@@ -1,41 +1,23 @@
 <?php session_start(); ?>
-<?php include('fonctions.php');?>
 
-<?php //--------------------------Récupération des articles à afficher --------------
-    //on récupère le numéro de page
-    $num_page = numero_page();
+<?php require('modele.php'); ?>
 
-    //on définit le nombre d'articles par page 
-    $articles_par_page = 2;
-
-    //Puis on calcul le n° de l'article qui doit s'afficher en premier
-    $num_article = ($num_page-1)*$articles_par_page;
-
-    //on se connecte à la bdd
-    require('connecter-bdd.php');
-
-    //on prépare la requête de récupération des articles
-    $sql = 'SELECT id, titre, contenu, DATE_FORMAT(date_creation,  \'%d/%m/%Y %H:%i:%s\') AS date_creation_fr FROM articles ORDER BY date_creation DESC LIMIT ' . $num_article . ',' . $articles_par_page . '';
-    $requete = $bdd->prepare($sql);
-
-    //on execute la requête préparée
-    $requete->execute();
+<?php //on définit le numéro de la page
+    if (isset($_GET['num_page']))
+    {
+        $num_page = numeroPage($_GET['num_page']);
+    } else {
+        $num_page = 1;
+    }
+?>
+    
+<?php //on définit le nombre d'articles par page 
+    $articles_par_page = articlesParPage(2);
 ?>
 
-<?php  //----------------------------------------------------------Création de la pagination-------
-    //on prépare la requête de récupération de tous les articles 
-    $sql = 'SELECT COUNT(*) AS nb_billets FROM articles';
-    $req = $bdd->prepare($sql);
 
-    //on exécute la requête préparée
-    $req->execute();
+<?php $nbr_pages = nombrePages($articles_par_page);?>
 
-    //on récupère le nombre d'articles
-    $res = $req->fetch();
-    $nbr_articles = $res['nb_billets'];
-
-    //on déduit le nombre de pages nécessaires
-    $nbr_pages = ceil(($nbr_articles/$articles_par_page));
-?>
+<?php $requete = recupererArticles($num_page,$articles_par_page); ?>
 
 <?php require('afficher-accueil.php'); ?>
---
+++ tp-blog-MVC/modele.php
@@ -0,0 +1,78 @@
+<?php
+function recupererArticles($num_p,$art_p)
+{
+    //on récupère le numéro de page
+    $num_page = $num_p;
+
+    //on définit le nombre d'articles par page 
+    $articles_par_page = $art_p;
+
+    //Puis on calcul le n° de l'article qui doit s'afficher en premier
+    $num_article = ($num_page-1)*$articles_par_page;
+
+    //on se connecte à la bdd
+    require('connecter-bdd.php');
+
+    //on prépare la requête de récupération des articles
+    $sql = 'SELECT id, titre, contenu, DATE_FORMAT(date_creation,  \'%d/%m/%Y %H:%i:%s\') AS date_creation_fr FROM articles ORDER BY date_creation DESC LIMIT ' . $num_article . ',' . $articles_par_page . '';
+
+    $requete = $bdd->prepare($sql);
+
+    //on place la requête exécutée dans une variable
+    $requete->execute();
+
+    //on retourne la requête exécutée
+    return $requete;
+}
+
+function numeroPage($get)
+{
+        //on réduit la faille XSS de la donnée récupérée
+        $get = htmlspecialchars($get);
+
+        //on s'assure du bon typage de la donnée
+        $num_page = (int)$get;
+
+        //si le numéro de page est supérieur à 100
+        if($num_page > 100)
+        {   //on affiche un message d'erreur
+            ?>
+                <section class="bloc center padding"  style="text-align:center;">
+                <h2>Bizarre ce numéro de page...</h2>
+                </section>
+            <?php
+         }
+
+    return $num_page;
+}
+
+function articlesParPage($nbr)
+{
+    $articles_par_page = $nbr;
+    return $articles_par_page;
+}
+
+function nombrePages($art_p)
+{
+
+    $articles_par_page = $art_p;
+    //on prépare la requête de récupération de tous les articles
+
+    require('connecter-bdd.php');
+
+    $sql = 'SELECT COUNT(*) AS nb_articles FROM articles';
+    $req = $bdd->prepare($sql);
+
+    //on exécute la requête préparée
+    $req->execute();
+
+    //on récupère le nombre d'articles
+    $res = $req->fetch();
+    $nbr_articles = $res['nb_articles'];
+
+    //on déduit le nombre de pages nécessaires
+    $nbr_pages = ceil(($nbr_articles/$articles_par_page));
+
+    return $nbr_pages;
+}
+?>
