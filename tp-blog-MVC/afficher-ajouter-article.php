0671ccf2d1be9f1a33a1e17e8ef59002b0511c67
Christophe Frignet <chrisfrignet@gmail.com>
2020-11-15 22:40:50 +0100

    Rectifications indentation

--- tp-blog-MVC/afficher-ajouter-article.php
+++ tp-blog-MVC/afficher-ajouter-article.php
@@ -9,21 +9,21 @@
 
 <body>
 
-<p style="text-align: center;"><a href="index.php"><< Retour à l'accueil</a></p>
+    <p style="text-align: center;"><a href="index.php"><< Retour à l'accueil</a></p>
 
-<h1>Ajouter article</h1>
+    <h1>Ajouter article</h1>
 
-<section class="bloc center padding">
-    <form action="ajouter-article-traitement.php" method="post">
+    <section class="bloc center padding">
+        <form action="ajouter-article-traitement.php" method="post">
 
-        <label for="titre">Titre de l'article</label><br>
-        <input type="text" id="titre" name="titre" value="Titre de l'article"><br><br>
+            <label for="titre">Titre de l'article</label><br>
+            <input type="text" id="titre" name="titre" value="Titre de l'article"><br><br>
 
-        <label for="contenu">Contenu de l'article</label><br>
-        <textarea id="contenu" name="contenu" rows="10" cols="100">Contenu de l'article</textarea><br><br>
+            <label for="contenu">Contenu de l'article</label><br>
+            <textarea id="contenu" name="contenu" rows="10" cols="100">Contenu de l'article</textarea><br><br>
 
-        <input type="submit" value="Valider article">
-    </form>
-</section>
+            <input type="submit" value="Valider article">
+        </form>
+    </section>
 
 </body>
